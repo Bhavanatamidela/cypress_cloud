dbbc61907d361402e8cffa3da26d5c38757f1eff
bhavana tamidela <tbhavana291@gmail.com>
2025-11-27 12:12:26 +0530

    msg

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "zk493c",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "399iks"
+    
   },
 });
